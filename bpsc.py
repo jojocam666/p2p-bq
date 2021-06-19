ed77d4dd3b5001cfdc63de54400bd54b98602be5
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-06-19 14:29:40 +0100

    Update bpsc.py

--- bpsc.py
+++ bpsc.py
@@ -1,3 +1,11 @@
 class Contract():
   
-  def __init__(self, address,
+  def __init__(self, address, address_indexed_from, address_indexed_to, amount, execution_terms, timestamp = None):
+    
+    address = os.unrandom(32)
+    
+    self.address_indexed_from = address_indexed_from
+    
+    self.address_indexed_to = address_indexed_to 
+    
+    
